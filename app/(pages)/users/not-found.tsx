d99e57836459cac3b1c7b8851b69abf2c7de1618
Michal Ciberej <michal.ciberej@gmail.com>
2023-11-01 01:18:19 +0200

    Added notFound page

---
+++ app/(pages)/users/not-found.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <main>
+      <section>
+        <div>awdjoaw</div>
+      </section>
+    </main>
+  );
+};
+
+export default NotFoundPage;
