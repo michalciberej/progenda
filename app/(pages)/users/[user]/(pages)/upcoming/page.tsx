ae67262283aed69b712a550778c8f51c62b44802
Michal Ciberej <michal.ciberej@gmail.com>
2023-11-02 20:07:20 +0200

    Refactored styles

--- app/(pages)/users/[user]/(pages)/upcoming/page.tsx
+++ app/(pages)/users/[user]/(pages)/upcoming/page.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import TaskContainer from '@/app/components/TaskContainer';
+import getTasks from '@/app/actions/getTasks';
+import filterTasks from '@/app/lib/filterTasks';
 
-const UpcomingPage = () => {
+const UpcomingPage = async () => {
+  const tasks = await getTasks();
+  const todayTasks = filterTasks(tasks, 'today');
+
   return (
     <div className='w-full h-full flex flex-col space-y-6'>
       <section>
         <h1 className='text-5xl relative z-10 text-text_LM my-4'>Upcoming</h1>
       </section>
-      <div className='h-full grid md:grid-cols-2 md:grid-rows-2 gap-6'>
+      <div className='overflow-auto flex flex-col space-y-6'>
         <TaskContainer
+          tasks={todayTasks}
           title='Today'
-          spanFullWidth
+          span
         />
-        <TaskContainer title='Tomorrow' />
-        <TaskContainer title='This Week' />
+        <TaskContainer
+          title='Tomorrow'
+          tasks={tasks}
+        />
+        <TaskContainer
+          title='Total'
+          tasks={tasks}
+        />
       </div>
     </div>
   );
