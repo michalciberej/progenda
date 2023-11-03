f819c1121866859da9a871fbbd3090f88ab4f631
Michal Ciberej <michal.ciberej@gmail.com>
2023-11-04 00:00:48 +0200

    Added tomorrow and nextWeekDay

--- app/lib/getCurrentDate.ts
+++ app/lib/getCurrentDate.ts
@@ -1,18 +1,34 @@
 const getCurrentDate = () => {
   let today: string | Date = new Date();
   let day: number | string = today.getDate();
+  let tomorrowDay: number | string = today.getDate() + 1;
+  let nextWeekDay: number | string = today.getDate() + 7;
   let month: number | string = today.getMonth() + 1;
   let year = today.getFullYear();
 
   if (day < 10) {
     day = '0' + day;
   }
+
   if (month < 10) {
     month = '0' + month;
   }
-  today = year + '-' + month + '-' + day;
 
-  return { today, day, month, year };
+  if (tomorrowDay < 10) {
+    tomorrowDay = '0' + tomorrowDay;
+  }
+
+  if (nextWeekDay < 10) {
+    nextWeekDay = '0' + nextWeekDay;
+  }
+
+  today = year + '-' + month + '-' + day;
+  const tomorrow = year + '-' + month + '-' + tomorrowDay;
+  const todayNextWeek = year + '-' + month + '-' + nextWeekDay;
+
+  return { today, tomorrow, todayNextWeek, day, month, year };
 };
 
 export default getCurrentDate;
+
+// TODO WRITE CONDITION THAT UPDATES WHEN DATE + X = NEXT MONTH
