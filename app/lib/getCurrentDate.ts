c6ce3d93f92777627af62d17aa3e0bc03d5cc76a
Michal Ciberej <michal.ciberej@gmail.com>
2023-11-15 23:40:44 +0200

    Fix bug to get correct date when end of the month

--- app/lib/getCurrentDate.ts
+++ app/lib/getCurrentDate.ts
@@ -1,34 +1,33 @@
 const getCurrentDate = () => {
   let today: string | Date = new Date();
-  let day: number | string = today.getDate();
-  let tomorrowDay: number | string = today.getDate() + 1;
-  let nextWeekDay: number | string = today.getDate() + 7;
-  let month: number | string = today.getMonth() + 1;
-  let year = today.getFullYear();
+  let day: string | number = today.getDate();
+  let month: string | number = today.getMonth() + 1;
+  const year = today.getFullYear();
 
-  if (day < 10) {
-    day = '0' + day;
-  }
+  let tomorrow: string | Date = new Date(today);
+  tomorrow.setDate(today.getDate() + 1);
+  let tmrwDay: string | number = tomorrow.getDate();
+  let tmrwMonth: string | number = tomorrow.getMonth() + 1;
+  const tmrwYear = tomorrow.getFullYear();
 
-  if (month < 10) {
-    month = '0' + month;
-  }
+  let todayNextWeek: string | Date = new Date(today);
+  todayNextWeek.setDate(today.getDate() + 7);
+  let nextWeekDay: string | number = todayNextWeek.getDate();
+  let nextWeekMonth: string | number = todayNextWeek.getMonth() + 1;
+  const nextWeekYear = todayNextWeek.getFullYear();
 
-  if (tomorrowDay < 10) {
-    tomorrowDay = '0' + tomorrowDay;
-  }
-
-  if (nextWeekDay < 10) {
-    nextWeekDay = '0' + nextWeekDay;
-  }
+  if (day < 10) day = '0' + day;
+  if (month < 10) month = '0' + month;
+  if (tmrwDay < 10) tmrwDay = '0' + tmrwDay;
+  if (tmrwMonth < 10) tmrwMonth = '0' + tmrwMonth;
+  if (nextWeekDay < 10) nextWeekDay = '0' + nextWeekDay;
+  if (nextWeekMonth < 10) nextWeekMonth = '0' + nextWeekMonth;
 
   today = year + '-' + month + '-' + day;
-  const tomorrow = year + '-' + month + '-' + tomorrowDay;
-  const todayNextWeek = year + '-' + month + '-' + nextWeekDay;
+  tomorrow = tmrwYear + '-' + tmrwMonth + '-' + tmrwDay;
+  todayNextWeek = nextWeekYear + '-' + nextWeekMonth + '-' + nextWeekDay;
 
   return { today, tomorrow, todayNextWeek, day, month, year };
 };
 
 export default getCurrentDate;
-
-// TODO WRITE CONDITION THAT UPDATES WHEN DATE + X = NEXT MONTH
