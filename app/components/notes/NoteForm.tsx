f6ba75fcd22be55d8c95942c7e8dcabf7a8b5992
Michal Ciberej <michal.ciberej@gmail.com>
2023-11-18 18:07:09 +0200

    Refactored structure and styles

--- app/components/notes/NoteForm.tsx
+++ app/components/notes/NoteForm.tsx
@@ -22,12 +22,12 @@ const NoteForm = () => {
   const [noteData, setNoteData] = useState<NoteData>({
     title: '',
     body: '',
-    color: '',
+    color: 'f87171',
   });
 
   return (
-    <li className='bg-secondary_LM dark:bg-secondary_DM w-full h-full rounded-xl flex flex-col p-6 shadow-md dark:shadow-none relative overflow-hidden justify-between'>
-      <form className='space-y-4'>
+    <li className=' w-full h-full rounded-xl flex flex-col p-4 shadow-md dark:shadow-none relative'>
+      <form className='flex flex-col justify-between h-full space-y-4'>
         <input
           type='text'
           placeholder='Title*'
@@ -41,7 +41,7 @@ const NoteForm = () => {
           onChange={(e) =>
             setNoteData({ ...noteData, body: e.currentTarget.value })
           }
-          className='w-full h-full bg-transparent max-h-[8rem] border-2 border-background_DM/20 dark:border-background_LM/20 p-2 rounded-lg resize-none placeholder:text-text_LM/50 dark:placeholder:text-text_DM/50'
+          className='w-full h-20 lg:h-28 bg-transparent max-h-[8rem] border-2 border-background_DM/20 dark:border-background_LM/20 p-2 rounded-lg resize-none placeholder:text-text_LM/50 dark:placeholder:text-text_DM/50'
         />
         <div className='flex justify-between items-center px-1'>
           {colors.map((color, index) => (
@@ -58,27 +58,29 @@ const NoteForm = () => {
             />
           ))}
         </div>
+        <StyledButton
+          primary
+          fullWidth
+          type='button'
+          func={() => {
+            axios
+              .post('/api/createNote', noteData)
+              .then(() => {
+                toast.success('Note created!');
+                setNoteData({ title: '', body: '', color: '' });
+                setShowForm(false);
+              })
+              .catch(() => toast.error('Something went wrong!'));
+          }}>
+          Create Note
+        </StyledButton>
       </form>
-      <StyledButton
-        primary
-        func={() => {
-          axios
-            .post('/api/createNote', noteData)
-            .then(() => {
-              toast.success('Note created!');
-              setShowForm(false);
-              setNoteData({ title: '', body: '', color: '' });
-            })
-            .catch(() => toast.error('Something went wrong!'));
-        }}>
-        Create Note
-      </StyledButton>
       {!showForm && (
         <button
           type='button'
           aria-label='Add note'
           onClick={() => setShowForm(!showForm)}
-          className='text-8xl absolute w-full inset-0 z-50 bg-secondary_LM dark:bg-secondary_DM '>
+          className='text-8xl absolute w-full inset-0 z-10 bg-secondary_LM dark:bg-secondary_DM rounded-xl'>
           +
         </button>
       )}
