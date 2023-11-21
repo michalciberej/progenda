1d3c2c113b7c232065819ab75850b26569b7e965
Michal Ciberej <michal.ciberej@gmail.com>
2023-11-21 18:19:57 +0200

    Removed page

--- app/(pages)/user/page.tsx
+++
@@ -1,9 +0,0 @@
-const UserPage = () => {
-  return (
-    <div className='border border-background_DM/10 dark:border-background_LM/10 rounded-xl w-full h-full'>
-      <section></section>
-    </div>
-  );
-};
-
-export default UserPage;
